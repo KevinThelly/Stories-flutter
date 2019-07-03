3e01556e63c909114e1c7d20eb6a1558eb8004d1
KevinThelly <kevin.thelly@gmail.com>
2019-07-04 00:51:31 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'customIcons.dart';
+
+void main() => runApp(MaterialApp(
+  home: MyApp(),
+  debugShowCheckedModeBanner: false,
+));
+
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new  _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  
+  @override
+  Widget build(BuildContext context) {
+  
+    return Scaffold(
+      backgroundColor: Color(0xFF2d3447),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 12.0, right: 12.0,top: 30.0, bottom: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(
+                      Icons.menu,
+                      color: Colors.white,
+                      size:30.0,
+                    ),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      Icons.search,
+                      color: Colors.white,
+                      size:30.0,
+                    ),
+                    onPressed: () {},
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text("Trending",style:TextStyle(
+                    color: Colors.white,
+                    fontSize: 46.0,
+                    fontFamily: "Calibre-Semibold",
+                    letterSpacing: 1.0,
+                  )),
+                  IconButton(
+                    icon: Icon(
+                      Icons.apps,
+                      size: 12.0,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {},
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xFFff6e6e),
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    child: Center(
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 22.0, vertical: 6.0),
+                        child: Text(
+                          "Animated",
+                          style:TextStyle(color: Colors.white)
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 15.0),
+                  Text("25 + Stories",)
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
